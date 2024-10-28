ea64dfcc45eefc6ca42e1b1bba39fb5c27aef30f
prophetarmed <me@prophetarmed.com>
2024-10-28 21:55:44 +0000

    Add Stylua

--- home/common/neovim/default.nix
+++ home/common/neovim/default.nix
@@ -4,6 +4,8 @@
   ...
 }: {
   home.packages = with pkgs; [
+    stylua
+
     # plugin dependencies
     fd
     gcc
--- home/common/neovim/nvim/lua/plugins/lsp.lua
+++ home/common/neovim/nvim/lua/plugins/lsp.lua
@@ -35,7 +35,7 @@ return {
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-			for name, server in pairs(servers) do 
+			for name, server in pairs(servers) do
 				server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
 				require("lspconfig")[name].setup(server)
 			end
