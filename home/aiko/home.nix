6943c40e8cbd13cb34c581995989ee869f358d60
prophetarmed <me@prophetarmed.com>
2024-11-05 16:48:47 +0000

    Add some gnome settings

--- home/aiko/home.nix
+++ home/aiko/home.nix
@@ -16,6 +16,22 @@
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
     };
+    "org/gnome/desktop/peripherals/touchpad" = {
+      natural-scroll = false;
+    };
+    "org/gnome/settings-daemon/plugins/color" = {
+      night-light-enabled = true;
+    };
+    "org/gnome/wm/keybindings" = {
+      move-to-workspace-1 = [ "<Shift><Super>1" ];
+      move-to-workspace-2 = [ "<Shift><Super>2" ];
+      move-to-workspace-3 = [ "<Shift><Super>3" ];
+      move-to-workspace-4 = [ "<Shift><Super>4" ];
+      switch-to-workspace-1 = [ "<Super>1" ];
+      switch-to-workspace-2 = [ "<Super>2" ];
+      switch-to-workspace-3 = [ "<Super>3" ];
+      switch-to-workspace-4 = [ "<Super>4" ];
+    };
   };
 
   home.stateVersion = "24.05";
