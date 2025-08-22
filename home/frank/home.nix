73b9f6c096262876f35ab6064a0e78e1b40b2ec1
prophetarmed <me@prophetarmed.com>
2025-08-22 19:27:43 +0100

    Fix applications folder

--- home/frank/home.nix
+++ home/frank/home.nix
@@ -12,7 +12,11 @@
 
   home.packages = with pkgs; [man-pages whatsapp-for-mac xquartz];
 
-  home.file."Applications/PicardRemote.app".source = ./PicardRemote.app;
+  home.activation.copyPicardRemote = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    mkdir -p "$HOME/Applications"
+    rm -rf "$HOME/Applications/PicardRemote.app"
+    cp -R "${./PicardRemote.app}" "$HOME/Applications/PicardRemote.app"
+  '';
 
   programs.ssh = {
     enable = true;
